d17b2e442883cc107e205660191be1375f358e8a
Arthur Morais <arthrmrs@gmail.com>
2020-12-15 18:37:09 -0300

    preparando arquivo de configuração para database - preparando conexões a partir do arquivo

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -3,26 +3,29 @@
 /** Constantes Globais */
 define('ROOT', __DIR__ . '/../');
 
-/** Inicialização do Autoloader */
-require_once __DIR__ . '/../vendor/autoload.php';
+/** Autoloader */
+require_once ROOT . 'vendor/autoload.php';
+/** Funções Globais */
+require_once ROOT . 'bootstrap/globalfunctions.php';
+
+/** Dependências */
+
+use Noodlehaus\Config;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 /** Tratamento de Erros */
 \App\Handler\ErrorHandler::initHandler();
 
-/** Inicialização ORM */
-use \Illuminate\Database\Capsule\Manager as Capsule;
+/** Configurações */
+$dbconfig = new Config(ROOT . 'config/database.php');
+$connections = $dbconfig->get('connections');
 
+/** ORM - Preparando Conexões */
 $capsule = new Capsule;
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => '192.168.2.217',
-    'database'  => 'projetinho',
-    'username'  => 'root',
-    'password'  => '1234',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-], 'default');
+foreach ($connections as $name => $connection) {
+    $capsule->addConnection($connection, $name);
+}
+
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
--- config/database.php
+++ config/database.php
@@ -1,2 +1,15 @@
 <?php
-//TODO: Finalizar arquivo de configuração de Database
+return [
+    'connections' => [
+        'default' => [
+            'driver'    => 'mysql',
+            'host'      => '192.168.2.217',
+            'database'  => 'projetinho',
+            'username'  => 'root',
+            'password'  => '1234',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    ]
+];
